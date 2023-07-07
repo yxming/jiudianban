736214849365c980aa016ae4f461ffab20c08d8e
yangxm <snihje_01@163.com>
2023-07-08 07:12:40 +0800

    注释回收列表tabBar
    之前未测试方便回收列表放在tabBar，现从tabBar中移除，需要移除onShow函数中的getTabBar().init()操作。

--- page/recycleopt/pages/recycleaddrlist/recycleaddrlist.js
+++ page/recycleopt/pages/recycleaddrlist/recycleaddrlist.js
@@ -78,8 +78,8 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow() {
-      if(this.getTabBar())
-          this.getTabBar().init();
+      // if(this.getTabBar())
+      //     this.getTabBar().init();
     },
 
     /**
