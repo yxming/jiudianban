054b0cb74046c26f1cb124ec06abcea02f2aa9eb
yangxm <snihje_01@163.com>
2023-07-14 17:46:06 +0800

    update

--- page/discount/discount.js
+++ page/discount/discount.js
@@ -1,22 +1,22 @@
-// pages/discount/discount.js
+// // pages/discount/discount.js
 Page({
     onShow: function () {
-        // 检查功能是否开放
-        // if (!getApp().globalData.tabBarEnabled) {
-          // 功能未开放，提示用户并跳转离开
-          wx.showToast({
-            title: '功能暂未开放',
-            icon: 'loading',
-            duration: 5000,
-            complete: function () {
-             setTimeout(() => {
-                wx.switchTab({
-                    url: '/page/home/home' // 跳转到其他页面
-                  });
-             }, 500);
-            }
-          });
-        }
-    //   }
+//         // 检查功能是否开放
+//         // if (!getApp().globalData.tabBarEnabled) {
+//           // 功能未开放，提示用户并跳转离开
+//           wx.showToast({
+//             title: '功能暂未开放',
+//             icon: 'loading',
+//             duration: 5000,
+//             complete: function () {
+//              setTimeout(() => {
+//                 wx.switchTab({
+//                     url: '/page/home/home' // 跳转到其他页面
+//                   });
+//              }, 500);
+//             }
+//           });
+//         }
+      }
       
 })
