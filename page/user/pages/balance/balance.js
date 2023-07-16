21ab56d54aeee7de7799837fc784c71453f52b20
yangxm <snihje_01@163.com>
2023-07-16 20:53:10 +0800

    Update balance.js
    remove useless data code

--- page/user/pages/balance/balance.js
+++ page/user/pages/balance/balance.js
@@ -1,21 +1,11 @@
 // page/user/balance/balance.js
 const util = require('../../../../util/util.js')
-var idinfolist = [
-    { "code": "结果", "text": '' },
-    { "code": "省", "text": '' },
-    { "code": "市", "text": '' },
-    { "code": "县", "text": ''},
-    { "code": "性别", "text": ''},
-    { "code": "出生年月", "text": ''},
-    { "code": "地址", "text": ''}
-   ]
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-        listData: idinfolist,  
         inputValue: '', //用于显示输入语句
         searchinput: '', //用户输入的查询语
         cashrecord:[],
