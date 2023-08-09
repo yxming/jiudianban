6c7d6d878402fddff88d3cb961e26a2cf3dd75cd
HhOHZ <2429202866@qq.com>
2023-08-09 19:06:16 +0800

    更新 recycleorders.js

--- page/recycleorders/recycleorders.js
+++ page/recycleorders/recycleorders.js
@@ -158,6 +158,21 @@ Page({
           }
         })
     },
+
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+      
+    },
     switchtwe(e) {
       const id = e.currentTarget.id
       const orderlist = this.data.orderlist
