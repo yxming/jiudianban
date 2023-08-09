262cc51823ea3d43290f78597ec146f5d4bfe28b
yangxm <snihje_01@163.com>
2023-08-09 19:08:43 +0800

    Update recycleorders.js

--- page/recycleorders/recycleorders.js
+++ page/recycleorders/recycleorders.js
@@ -158,21 +158,7 @@ Page({
           }
         })
     },
-
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-      
-    },
+    
     switchtwe(e) {
       const id = e.currentTarget.id
       const orderlist = this.data.orderlist
